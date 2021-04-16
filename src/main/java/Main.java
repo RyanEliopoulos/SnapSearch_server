ab8978aca2ce9cdf608e26c5a9aae782be18dff8
Ryan <ryan.eliopoulos@gmail.com>
2021-04-15 21:14:06 -0700

    database read/write++
    Working PoC.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import testpackage.SnapListener;
 import testpackage.DBInstantiator;
 import testpackage.DBInterface;
+
+import java.io.*;
 import java.util.HashMap;
 import java.util.Set;
 
@@ -10,20 +12,64 @@ public class Main {
         // Connect to database
         DBInterface iface = new DBInterface("latest.db");
         iface.connect();
-//        iface.directcontrol();
 
-        // Pulling photos belonging to user 1
+        /*
+
+            This block reads the images and inserts it into the database
+         */
+//        try {
+//            // Open file
+//            FileInputStream fis = new FileInputStream("C:/Users/Ryan/Mickey_Mouse.png");
+//
+//            // Read file into buffer
+//            ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+//            int bt;
+//            while((bt = fis.read()) != -1) {
+//                outstream.write(bt);
+//            }
+//
+//           //
+//            byte[] filedata = outstream.toByteArray();
+//            if (iface.insertPhoto(1, filedata) == -1) {
+//                System.out.println("Call to insert_photos failed");
+//            }
+//        }
+//        catch (FileNotFoundException e ) {
+//            e.printStackTrace();
+//        }
+//        catch (IOException io) {
+//            io.printStackTrace();
+//        }
+
+
+        /*
+            This block is for pulling photos from the table. Works.
+
+         */
         HashMap<String, byte[]> hm = iface.getPhotos(1);
 
         Set st = hm.keySet();
         String[] sa = (String[]) st.toArray(new String[0]);  // Intellij says must cast. Docs don't..
 
-
         for (String key : sa) {
-            // For each string (photoid)
             System.out.println(key);
         }
 
+
+
+        // Attempting to write one of the photos to a file
+        try {
+            FileOutputStream fos = new FileOutputStream("C:/Users/Ryan/NewMouse.png");
+            byte[] filedata = hm.get("2");
+            fos.write(filedata);
+
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e ) {
+            e.printStackTrace();
+        }
 
 
 //        SnapListener sl = new SnapListener();
